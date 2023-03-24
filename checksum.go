773bdac62539f8ee3e62d598e052f407808e7e4a
Diogo Bersuc <dbersuc@gmail.com>
2023-03-24 09:32:57 -0300

    Checksum calculates from file

--- checksum.go
+++ checksum.go
@@ -1,30 +1,55 @@
 /*
-go run checksum.go "8=FIX.4.2|9=49|35=5|34=1|49=ARCA|52=20150916-04:14:05.306|56=TW|"
+Add one messages per line as example on messages.txt.
+go run checksum.go
 Reference : https://stackoverflow.com/questions/32708068/how-to-calculate-checksum-in-fix-manually
 */
 
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	var soma = 0
-	if len(os.Args) < 2 {
-		log.Fatal("Need to provide fix message as argument")
-	}
-	var arg1 = os.Args[1]
-	for _, c := range arg1 {
-		if string(c) == "|" {
-			soma += 1
-			continue
-		} else {
-			soma += int(c)
-		}
-	}
-	fmt.Printf("%s10=%d", arg1, soma%256)
+	readFile()
+}
 
+// Read the messages.txt file and send to Chacksum calculation function
+func readFile() {
+	mydir, err := os.Getwd()
+	check(err)
+	infos, err := os.ReadFile(mydir + "\\messages.txt")
+	check(err)
+	messages := strings.Split(string(infos), "\r\n")
+	calcCheckSum(messages)
+}
+
+// Get the slice of strings from the messages.txt file and print on console
+func calcCheckSum(args []string) {
+	if len(args) < 1 {
+		err := errors.New("No line in file")
+		check(err)
+	}
+	for _, message := range args {
+		var soma = 0
+		for _, c := range message {
+			if string(c) == "|" {
+				soma += 1
+				continue
+			} else {
+				soma += int(c)
+			}
+		}
+		fmt.Printf("%s10=%d\n", message, soma%256)
+	}
+}
+
+// Send an error to this function.
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
